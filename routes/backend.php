459321a2950e2af9359b8b3d0da208ab1bbe9095
Name <you@example.com>
2024-12-28 19:42:59 +0000

    Pass Auth Test

--- routes/backend.php
+++ routes/backend.php
@@ -1,7 +1,16 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
 
 Route::controller(UserController::class)->group(function(){
     Route::get('users', 'index');
